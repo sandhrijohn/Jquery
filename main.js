6837110b6681ec24ef3d988d266fa4af473d0223
SANDHRI JOHN ABHISHEK <asandhrijohn@gmail.com>
2024-12-23 11:03:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $("#range-slider").on("input change", function () {
+      $(".text").css("font-size", $(this).val() + "px");
+      //change span value
+      $(".value").text($(this).val() + "px");
+    });
+  });
+  
+  
